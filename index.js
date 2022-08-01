4ae6f77e3512e66f0edd9a6dc235c78957ba8041
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-01 15:03:20 +0600

    add kilometers to miles

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-// function 
+// function inch to feet 
 function inchToFeet(inch) {
     var feet = inch / 12;
-    return feet
+    return feet;
 }
-var lenghtInch = 2682;
-var lengthResult = inchToFeet(lenghtInch);
-console.log(lengthResult); 
+var amountInch = 2682;
+var feetResult = inchToFeet(amountInch);
+console.log(feetResult);
+
+// function kilometers to miles 
+function kilometersToMiles(kilometers) {
+    var miles = kilometers / 1.60;
+    return miles;
+}
+var amountKilometers = 1352;
+var milesResult = kilometersToMiles(amountKilometers);
+console.log(milesResult);
+
