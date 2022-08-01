2add36fb081a17011854cf897664f0f4886ff9dd
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-01 15:44:47 +0600

    add event and odd number function

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ var amountKilometers = 1352;
 var milesResult = kilometersToMiles(amountKilometers);
 console.log(milesResult);
 
+// even number function 
+function evenNumber(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+var bikeNumebr = 3652;
+var bikeResult = evenNumber(bikeNumebr);
+console.log(bikeResult);
+
+// odd number fucntion 
+function oddNumber(number) {
+    if (number % 2 != 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+var carNumebr = 361;
+var carResult = oddNumber(carNumebr);
+console.log(carResult); 
