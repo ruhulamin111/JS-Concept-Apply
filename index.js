489fd76825301cda0e24a3fdbff523e8bf584d37
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-01 14:34:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// function 
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet
+}
+var lenghtInch = 2682;
+var lengthResult = inchToFeet(lenghtInch);
+console.log(lengthResult); 
